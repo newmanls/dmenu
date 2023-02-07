56bb474a3e2c8f51c19394f9d451fe6be51fe269
lr-tech <lrtech8@gmail.com>
2023-02-07 09:57:32 -0400

    update default settings

--- config.h
+++ config.h
@@ -1,15 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 1;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=10";
+static char font[] = "monospace-10";
 static const char *fonts[] = {
     font,
     "Noto Color Emoji-9"
 };
 
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* -p  option; prompt to the left of input field */
+static char *prompt = NULL;
 
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
@@ -21,23 +24,20 @@ static char *colors[SchemeLast][2] = {
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
 	[SchemeOut]  = { "#000000",   "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines = 0;
+
+/* Characters not considered part of a word while deleting words */
+/* for example: " /?\"&[]" */
 static const char worddelimiters[] = " ";
 
-/*
- * Xresources preferences to load at startup
- */
+/* Xresources preferences to load at startup */
 ResourcePref resources[] = {
 	{ "font",        STRING, &font },
 	{ "prompt",      STRING, &prompt },
-	{ "background",  STRING, &normbgcolor },
+	{ "color0",      STRING, &normbgcolor },
 	{ "foreground",  STRING, &normfgcolor },
-	{ "color0",      STRING, &selbgcolor },
-	{ "accentColor", STRING, &selfgcolor },
+	{ "background",  STRING, &selbgcolor },
+	{ "foreground",  STRING, &selfgcolor },
 };
